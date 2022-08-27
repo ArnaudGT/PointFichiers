706314ed95443dfd95d0a823a281fecdbd0465d1
ArnaudGT <93054055+ArnaudGT@users.noreply.github.com>
2022-08-28 00:55:16 +0200

    Update Manjaro.sh

--- Manjaro.sh
+++ Manjaro.sh
@@ -64,23 +64,11 @@ for PKG in "${PKGS[@]}"; do
     sudo pacman -S "$PKG" --noconfirm --needed
 done
 
-echo "---------"
-echo "// AUR //"
-echo "---------"
-
-PKGS=(
-'librewolf-bin'
-)
-
-for PKG in "${PKGS[@]}"; do
-    yay -S --noconfirm $PKG
-done
-
 echo "---------------------------------------"
 echo "// Installations des packets Flatpak //"
 echo "---------------------------------------"
 
-sh Flatpak.sh
+sh ~/PointFichiers/Flatpak.sh
 
 echo "-------------"
 echo "// Konsave //"
@@ -88,16 +76,50 @@ echo "-------------"
 
 pip install konsave
 konsave --noconfirm
-cd ~/installation/
-konsave -i pointfichiers.knsv
+cd ~/PointFichiers/
+konsave -i Argama.knsv
 cd ~
 sleep 1
-konsave -a pointfichiers
+konsave -a Argama
 
 echo "----------------"
 echo "// Youtube-DL //"
 echo "----------------"
 
 python3 -m pip install -U yt-dlp
+
+echo "--------------------"
+echo "// Drivers NVIDIA //"
+echo "--------------------"
+ 
+if lspci | grep -E "NVIDIA|GeForce"; then
+    sudo mhwd -a pci nonfree 0300
+fi
+
+echo "--------------------"
+echo "// Fichiers Hosts //"
+echo "--------------------"
+
+git clone https://github.com/StevenBlack/hosts.git ~/PointFichiers/Hosts 
+cd ~/PointFichiers/Hosts
+echo "#requirements.txt"
+pip3 install --user -r requirements.txt
+echo "#testUpdateHostsFile.py"
+python3 testUpdateHostsFile.py
+echo "#updateHostsFile.py"
+python3 updateHostsFile.py -e gambling
+cd ~
+
+echo "--------------------"
+echo "// XP-Pen Drivers //"
+echo "--------------------"
+
+cd ~/PointFichiers/
+wget -O xp-pen.tar.gz --referer https://www.xp-pen.fr/ 'https://www.xp-pen.fr/download/file/id/1945/pid/292/ext/gz.html'
+mkdir ~/PointFichiers/XP-pen
+tar -x -f  xp-pen.tar.gz -C ~/PointFichiers/XP-pen --strip-components 1
+cd ~/PointFichiers/XP-pen
+sudo sh install.sh
+cd ~
 
 exit
