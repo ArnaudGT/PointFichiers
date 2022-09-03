cca619f7de43d8dd8f909941036b0dcf975b50ec
ArnaudGT <93054055+ArnaudGT@users.noreply.github.com>
2022-09-03 05:15:43 +0200

    Update XP-Pen-Drivers.sh

--- Scripts-Installation/XP-Pen-Drivers.sh
+++ Scripts-Installation/XP-Pen-Drivers.sh
@@ -2,8 +2,8 @@
 
 cd ~/PointFichiers/
 wget -O xp-pen.tar.gz --referer https://www.xp-pen.fr/ 'https://www.xp-pen.fr/download/file/id/1945/pid/292/ext/gz.html'
-mkdir ~/PointFichiers/XP-pen
-tar -x -f  xp-pen.tar.gz -C ~/PointFichiers/XP-pen --strip-components 1
-cd ~/PointFichiers/XP-pen
+mkdir ~/PointFichiers/Scripts-Installation/XP-pen
+tar -x -f  xp-pen.tar.gz -C ~/PointFichiers/Scripts-Installation/XP-pen --strip-components 1
+cd ~/PointFichiers/Scripts-Installation/XP-pen
 sudo sh install.sh
 cd ~
